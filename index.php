02566e87dfdfe94637ca19f8757deb95ef646da0
123paulsallu <magbieprincess@gmail.com>
2025-11-16 15:01:35 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ETUSL Web</title>
+    <link rel="stylesheet" href="/styles/logo.css">
+    <style>
+        body {
+            font-family: 'Italiana', 'Quicksand', Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <!-- Header Component -->
+    <?php include __DIR__ . '/components/HeaderComponent.html'; ?>
+</body>
+</html>
